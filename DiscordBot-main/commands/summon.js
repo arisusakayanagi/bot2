1caa67e20d00b5d14a218cebee556c6d7093e94b
arisusakayanagi <79960607+arisusakayanagi@users.noreply.github.com>
2021-03-17 00:37:52 -0500

    Delete DiscordBot-main/commands directory

--- DiscordBot-main/commands/summon.js
+++
@@ -1,9 +0,0 @@
-const { execute } = require("./ping");
-
-module.exports = {
-    name: 'summon',
-    description: 'Summon.',
-    execute(message, args){
-        message.channel.send('<@531285577853566996>');
-    }
-}
